3823016d654312cee115c52547aac1971a2ed85c
X <129300554+x6455@users.noreply.github.com>
2026-06-28 19:59:12 +0300

    Update user_introduction.dart

--- lib/widgets/user_introduction.dart
+++ lib/widgets/user_introduction.dart
@@ -16,7 +16,7 @@ class UserIntroduction extends StatelessWidget {
            
         ),
         Text(
-          "Selam, WALID",
+          "Selam, ABRAHAM",
           style: TextStyle(
             color: Colors.white70,
             fontSize: 18,
